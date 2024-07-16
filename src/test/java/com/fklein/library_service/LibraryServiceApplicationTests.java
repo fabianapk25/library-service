de242e6f377dc8bf14d2c9b54611e81e534c88ae
Fabiana Pagung Klein <102247585+fabianapk25@users.noreply.github.com>
2024-07-16 23:41:51 +0000

    added spring project

---
+++ src/test/java/com/fklein/library_service/LibraryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fklein.library_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
